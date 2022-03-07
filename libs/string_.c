7536d31945e17b2968f097dfed759a3516a819b2
Daniel Wahbi <xed255@gmail.com>
2022-03-07 21:47:16 +0300

    add all function from first example

--- libs/string_.c
+++ libs/string_.c
@@ -3,3 +3,104 @@
 //
 
 #include "string_.h"
+
+void printExitIfNullptr(const void *ptr) {
+    if (ptr == NULL) {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+}
+
+size_t strlen_(const char *begin) {
+    printExitIfNullptr(begin);
+
+    register char *end = begin;
+    while (*end != '\0')
+        end++;
+    return end - begin;
+}
+
+char *find(const char *begin, const char *const end, const int ch) {
+    printExitIfNullptr(begin);
+
+    while (begin != end && *begin != ch)
+        begin++;
+    return begin;
+}
+
+char *findNonSpace(const char *begin) {
+    printExitIfNullptr(begin);
+
+    while (*begin++ != '\0')
+        if (!isspace(*begin))
+            return begin;
+    return begin;
+}
+
+char *findSpace(const char *begin) {
+    printExitIfNullptr(begin);
+
+    while (*begin++ != '\0')
+        if (isspace(*begin))
+            return begin;
+    return begin;
+}
+
+char *findNonSpaceReverse(const char *rbegin, const char *rend) {
+    printExitIfNullptr(rbegin);
+
+    while (rbegin-- != rend)
+        if (!isspace(*rbegin))
+            return rbegin;
+    return rend;
+}
+
+char *findSpaceReverse(const char *rbegin, const char *rend) {
+    printExitIfNullptr(rbegin);
+
+    while (rbegin-- != rend)
+        if (isspace(*rbegin))
+            return rbegin;
+    return rend;
+}
+
+int strcmp_(const char *lhs, const char *rhs) {
+    printExitIfNullptr(lhs);
+    printExitIfNullptr(rhs);
+
+    for (; *lhs != '\0' || *rhs != '\0'; lhs++, rhs++) {
+        int differenceAddress = lhs - rhs;
+        if (differenceAddress != 0)
+            return differenceAddress;
+    }
+
+    return 0;
+}
+
+char *copy(const char *beginSource, const char *endSource, char *beginDestination) {
+    printExitIfNullptr(beginDestination);
+
+    memcpy(beginDestination, beginSource, (beginSource - endSource) * sizeof(char));
+
+    return beginDestination++;
+}
+
+char *copyIf(const char *beginSource, const char *endSource, char *beginDestination, int (*condition)(const int)) {
+    printExitIfNullptr(beginDestination);
+
+    while (beginSource++ != endSource)
+        if (condition(*beginSource))
+            *beginDestination++ = *beginSource;
+    return beginDestination++;
+}
+
+char *
+copyIfReverse(const char *rbeginSource, const char *rendSource, char *beginDestination, int (*condition)(const int)) {
+    printExitIfNullptr(beginDestination);
+
+    while (rbeginSource-- != rendSource)
+        if (condition(*rbeginSource))
+            *beginDestination++ = *rbeginSource;
+    return beginDestination++;
+}
+
--- libs/string_.h
+++ libs/string_.h
@@ -11,105 +11,27 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-void printExitIfNullptr(const void *ptr) {
-    if (ptr == NULL) {
-        fprintf(stderr, "bad alloc");
-        exit(1);
-    }
-}
+void printExitIfNullptr(const void *ptr);
 
-size_t strlen_(const char *begin) {
-    printExitIfNullptr(begin);
+size_t strlen_(const char *begin);
 
-    register char *end = begin;
-    while (*end != '\0')
-        end++;
-    return end - begin;
-}
+char *find(const char *begin, const char *const end, int ch);
 
-char *find(const char *begin, const char *const end, const int ch) {
-    printExitIfNullptr(begin);
+char *findNonSpace(const char *begin);
 
-    while (begin != end && *begin != ch)
-        begin++;
-    return begin;
-}
+char *findSpace(const char *begin);
 
-char *findNonSpace(const char *begin) {
-    printExitIfNullptr(begin);
+char *findNonSpaceReverse(const char *rbegin, const char *rend);
 
-    while (*begin++ != '\0')
-        if (!isspace(*begin))
-            return begin;
-    return begin;
-}
+char *findSpaceReverse(const char *rbegin, const char *rend);
 
-char *findSpace(const char *begin) {
-    printExitIfNullptr(begin);
+int strcmp_(const char *lhs, const char *rhs);
 
-    while (*begin++ != '\0')
-        if (isspace(*begin))
-            return begin;
-    return begin;
-}
+char *copy(const char *beginSource, const char *endSource, char *beginDestination);
 
-char *findNonSpaceReverse(const char *rbegin, const char *rend) {
-    printExitIfNullptr(rbegin);
-
-    while (rbegin-- != rend)
-        if (!isspace(*rbegin))
-            return rbegin;
-    return rend;
-}
-
-char *findSpaceReverse(const char *rbegin, const char *rend) {
-    printExitIfNullptr(rbegin);
-
-    while (rbegin-- != rend)
-        if (isspace(*rbegin))
-            return rbegin;
-    return rend;
-}
-
-int strcmp_(const char *lhs, const char *rhs) {
-    printExitIfNullptr(lhs);
-    printExitIfNullptr(rhs);
-
-    for (; *lhs != '\0' || *rhs != '\0'; lhs++, rhs++) {
-        int differenceAddress = lhs - rhs;
-        if (differenceAddress != 0)
-            return differenceAddress;
-    }
-
-    return 0;
-}
-
-char *copy(const char *beginSource, const char *endSource, char *beginDestination) {
-    printExitIfNullptr(beginDestination);
-
-    memcpy(beginDestination, beginSource, (beginSource - endSource) * sizeof(char));
-
-    return beginDestination++;
-}
-
-char *copyIf(const char *beginSource, const char *endSource, char *beginDestination, int (*condition)(const int)) {
-    printExitIfNullptr(beginDestination);
-
-    while (beginSource++ != endSource)
-        if (condition(*beginSource))
-            *beginDestination++ = *beginSource;
-    return beginDestination++;
-}
+char *copyIf(const char *beginSource, const char *endSource, char *beginDestination, int (*condition)(int));
 
 char *
-copyIfReverse(const char *rbeginSource, const char *rendSource, char *beginDestination, int (*condition)(const int)) {
-    printExitIfNullptr(beginDestination);
-
-    while (rbeginSource-- != rendSource)
-        if (condition(*rbeginSource))
-            *beginDestination++ = *rbeginSource;
-    return beginDestination++;
-}
-
+copyIfReverse(const char *rbeginSource, const char *rendSource, char *beginDestination, int (*condition)(int));
 
 #endif //LABA5E_STRING__H
